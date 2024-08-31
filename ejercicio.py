58c27f031b2bc09104692193a878bcf73b1f8600
JohanRamirez1618 <johanramirez1618@gmail.com>
2024-08-30 19:47:26 -0500

    En el siguiente codigo se realiza la tarea de calcular el area de un terreno triangular

---
+++ ejercicio.py
@@ -0,0 +1,7 @@
+#Participantes: Nestor Ivan Castellanos Merchan, Johan Yesid Ramirez Torres
+print ("Calcular area de un lote")
+base=int(input("Digite la base del terreno en metros cuadrados: "))
+altura=int(input("Digite la altura del terreno en metros cuadrados: "))
+
+areaTerreno = (base*altura)/2
+print(f"El area del terrreno es: {areaTerreno}m2")
